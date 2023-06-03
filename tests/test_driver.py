7aaafd4a8fa100820fccfddc38a182c7817c8c76
Eason <291028775@qq.com>
2023-06-04 03:05:56 +0800

    fix: finish

--- tests/test_driver.py
+++ tests/test_driver.py
@@ -4,4 +4,6 @@ from uia2.driver import AndroidDriver
 class TestDriver:
 
     def test_new_session(self):
-        AndroidDriver("http://localhost:6790")
+        driver = AndroidDriver("http://localhost:6790")
+        p = driver.get_page_source()
+        print(p)
--- uia2/driver.py
+++ uia2/driver.py
@@ -1,11 +1,17 @@
 from client.uia_client import UiaClient
+from common.resp_handler import RespHandler
 
 
 # Android Driver with uia client.
 class AndroidDriver(object):
 
-    def __init__(self, url):
+    def __init__(self, url, cap=None, timeout=RespHandler.DEFAULT_REQUEST_TIMEOUT):
+        if cap is None:
+            cap = {}
         self._client = UiaClient()
         self._client.remote_url = url
-        self._client.new_session({})
+        self._client.resp_handler.set_request_timeout(timeout)
+        self._client.new_session(cap)
 
+    def get_page_source(self):
+        return self._client.page_source()
